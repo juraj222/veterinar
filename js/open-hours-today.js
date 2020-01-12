0948cf11554821458a7d67867a3652f793966198
juraj <juraj.vrabec222@gmail.com>
2020-01-12 21:49:18 +0100

    fix day sunday is zero

--- js/open-hours-today.js
+++ js/open-hours-today.js
@@ -1,15 +1,16 @@
-var open_dubi = [ '18–19', '18–19', '18–19', '18–19', '18–19', '18–19', 'Zavřeno'  ];
-var open_duchcov = [ '9–11 14–16', '9–11 14–16:30', '9–11 14–16', '9–11 14–16:30', '9–11 14–16', 'Zavřeno', 'Zavřeno' ];
-var cz_week = ['pondělí', 'úterý', 'středa', 'čtvrtek', 'pátek', 'sobota', 'neděle'];
+var open_dubi = ['Zavřeno', '18–19', '18–19', '18–19', '18–19', '18–19', '18–19' ];
+var open_duchcov = ['Zavřeno', '9–11 14–16', '9–11 14–16:30', '9–11 14–16', '9–11 14–16:30', '9–11 14–16', 'Zavřeno' ];
+var cz_week = ['neděle', 'pondělí', 'úterý', 'středa', 'čtvrtek', 'pátek', 'sobota'];
 var actual_day = new Date().getDay();
-document.getElementById("Dubi-today").innerHTML = open_dubi[actual_day-1];
-document.getElementById("Duchcov-today").innerHTML = open_duchcov[actual_day-1];
+document.getElementById("Dubi-today").innerHTML = open_dubi[actual_day];
+document.getElementById("Duchcov-today").innerHTML = open_duchcov[actual_day];
 
 function generateWeekOpeningHoursTable(nameVet) {
     var result = "<table>";
-    for(i = 0; i < cz_week.length; i++) {
+    for(i = 1; i < cz_week.length; i++) {
     	result += '<tr><td>' + cz_week[i] + '</td>' + '<td>' + nameVet[i] + '</td></tr>';
     }
+    result += '<tr><td>' + cz_week[0] + '</td>' + '<td>' + nameVet[0] + '</td></tr>';
     return result += "</table>";
 }
 document.getElementById("dubi-week").innerHTML = generateWeekOpeningHoursTable(open_dubi);
