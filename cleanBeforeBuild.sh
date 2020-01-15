aaa7de3738bb662d7e2314e1a388aa73cf29395a
jurajvrabec <juraj.vrabec@itera.no>
2020-01-15 12:48:14 +0100

    cleanscript

--- cleanBeforeBuild.sh
+++ cleanBeforeBuild.sh
@@ -1,1 +1,6 @@
-docker rm $(docker stop $(docker ps -a -q --filter ancestor=webserver-image:v1 --format="{{.ID}}"))
+if [ "$(docker ps -a -q --filter ancestor=webserver-image:v1 --format="{{.ID}}")" -ge 1 ]
+then
+    docker rm $(docker stop $(docker ps -a -q --filter ancestor=webserver-image:v1 --format="{{.ID}}"))
+else
+    echo "docker already clean"
+fi
