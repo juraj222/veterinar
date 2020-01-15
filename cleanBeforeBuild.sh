ce25d2fe8ef4e4760aabccc4f25509c1c7276e90
jurajvrabec <juraj.vrabec@itera.no>
2020-01-15 12:57:48 +0100

    cleanbuildprocess

--- cleanBeforeBuild.sh
+++ cleanBeforeBuild.sh
@@ -1,6 +1,4 @@
-if [ "$(docker ps -a -q --filter ancestor=webserver-image:v1 --format="{{.ID}}")" -ge 1 ]
+if [ $(docker ps -a -q --filter ancestor=webserver-image:v1 --format="{{.ID}}") ]
 then
     docker rm $(docker stop $(docker ps -a -q --filter ancestor=webserver-image:v1 --format="{{.ID}}"))
-else
-    echo "docker already clean"
 fi
