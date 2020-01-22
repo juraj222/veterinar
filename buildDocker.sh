4b3e92ff15b6251d01e397392e8624f0ae62bfa0
jurajvrabec <juraj.vrabec@itera.no>
2020-01-22 13:50:45 +0100

    fix build compose

--- buildDocker.sh
+++ buildDocker.sh
@@ -1,2 +1,4 @@
-docker build -t webserver-image:v1 .
-docker run -d -p 80:80 webserver-image:v1
+# one docker image
+# docker build -t webserver-image:v1 .
+# docker run -d -p 80:80 webserver-image:v1
+docker-compose up
--- cleanBeforeBuild.sh
+++ cleanBeforeBuild.sh
@@ -1,6 +1,8 @@
-if [ $(docker ps -a -q --filter ancestor=webserver-image:v1 --format="{{.ID}}") ]
-then
-    docker rm $(docker stop $(docker ps -a -q --filter ancestor=webserver-image:v1 --format="{{.ID}}"))
-else
-    echo "docker already clean"
-fi
+# one docker image
+# if [ $(docker ps -a -q --filter ancestor=webserver-image:v1 --format="{{.ID}}") ]
+# then
+#     docker rm $(docker stop $(docker ps -a -q --filter ancestor=webserver-image:v1 --format="{{.ID}}"))
+# else
+#     echo "docker already clean"
+# fi
+docker-compose down --rmi all
